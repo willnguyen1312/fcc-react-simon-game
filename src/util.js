31fd4e06d5b8b5a79d15280f3d0202d2ae1a5ff4
Nam Nguyen <nlnam@fossil.com>
2018-03-06 14:40:31 +0700

    Clone the app

---
+++ src/util.js
@@ -0,0 +1,12 @@
+// root js, outside of any Component
+
+export default function getRandomIntInclusive(f, s) {
+  const min = Math.ceil(f)
+  const max = Math.floor(s)
+  // eslint-disable-next-line
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+const series = []
+for (let i = 0; i < 24; i += 1) {
+  series[i] = getRandomIntInclusive(0, 3)
+}
